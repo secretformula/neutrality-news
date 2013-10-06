891df30b6975962a59cd5e8663e7bd19c0cc757a
Wylie Conlon <wylieconlon@gmail.com>
2013-10-05 23:19:12 -0400

    Classify random articles from web

--- backend-web/app.js
+++ backend-web/app.js
@@ -4,10 +4,16 @@ var alchemy = require('../alchemyapi');
 var _ = require('underscore');
 var config = require('../config/config');
 var Gearman = require("node-gearman");
+var natural = require('natural');
+var fs = require('fs');
+var path = require('path');
 
 // Setup Connection to gearman
 var gearmanServer = new Gearman(config.gearman.host, config.gearman.port);
 gearmanServer.connect();
+
+var classifierDump = fs.readFileSync(path.join(__dirname, '..', 'processing-nlp', 'objectiveness-classifier.json'));
+var classifier = natural.BayesClassifier.restore(JSON.parse(classifierDump));
 
 var app = express();
 
@@ -26,7 +32,15 @@ app.post('/', function(req, res) {
   var url = req.param('url');
   
   alchemy.text_clean('url', url, {}, function(error, response) {
-    console.log(response.text.split("\n"));
+    var lines = response.text.split("\n");
+
+    for (var i=0; i<lines.length; i++) {
+      var line = lines[i];
+
+      var classified = classifier.classify(line);
+
+      console.log(classified, line);
+    }
 
     res.send("");
   });
