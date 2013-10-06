9adeb9f9ac8226809f3a907871bacb484d2c513c
Wylie Conlon <wylieconlon@gmail.com>
2013-10-05 22:42:31 -0400

    Read original text and annotation file to memory

---
+++ processing-nlp/processing-nlp.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var path = require('path');
+var _ = require('underscore');
+
+var root = path.join(__dirname, 'database.mpqa.2.0');
+
+var dirs = fs.readdirSync(path.join(root, 'docs'));
+
+var dates = _.filter(dirs, function(str) {
+  return str.match(/^[0-9]+$/)
+});
+
+console.log(dates);
+
+var sources = [];
+var annotations = [];
+
+for (var i=0; i<dates.length; i++) {
+  var sourceDir = fs.readdirSync(path.join(root, 'docs', dates[i]));
+
+  for (var j=0; j<sourceDir.length; j++) {
+    console.log(sourceDir[j]);
+    if (!sourceDir[j] || !sourceDir[j].match(/^[0-9]{2}\.[0-9]{2}\.[0-9]{2}\-[0-9]+$/)) {
+      continue;
+    }
+   
+    console.log(path.join(root, 'docs', dates[i], sourceDir[j]));
+    var source = fs.readFileSync(path.join(root, 'docs', dates[i], sourceDir[j]), { encoding: 'utf-8' });
+    var annotation = fs.readFileSync(path.join(root, 'man_anns', dates[i], sourceDir[j], 'gateman.mpqa.lre.2.0'), { encoding: 'utf-8' });
+
+    sources.push(source);
+    annotations.push(annotation);
+  }
+}
+
+console.log(sources);
